13f6f5e70d3b5c50357f09e1eeb2562755db25ac
Jhonny Frans Gallegos Mendoza <jgallegos@evolutionsoluciones.com>
2026-08-07 22:19:14 -0500

    stales stafeull

--- lib/pages/dice_page.dart
+++ lib/pages/dice_page.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class DicePage extends StatefulWidget {
@@ -7,6 +9,34 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
   int nDado = 1;
+
+  void aumentarDado() {
+    nDado = nDado == 6 ? 1 : nDado += 1;
+
+    // nDado = nDado + 1;
+    // if (nDado == 6) {
+    //   nDado = 1;
+    // } else {
+    //   nDado += 1;
+    // }
+    setState(() {});
+  }
+
+  void disminuirDado() {
+    nDado = nDado == 1 ? 6 : nDado -= 1;
+
+    // if (nDado == 1) {
+    //   nDado = 6;
+    // } else {
+    //   nDado -= 1;
+    // }
+    setState(() {});
+  }
+
+  void shuffleDado() {
+    nDado = Random().nextInt(6) + 1;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,12 +56,28 @@ class _DicePageState extends State<DicePage> {
               child: Image.asset("assets/images/dice$nDado.png"),
             ),
 
-            ElevatedButton(
-              onPressed: () {
-                nDado++;
-                setState(() {});
-              },
-              child: Text("Cambiar número de dado"),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    disminuirDado();
+                  },
+                  child: Icon(Icons.arrow_left_rounded, size: 50),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    shuffleDado();
+                  },
+                  child: Icon(Icons.shuffle, size: 50),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    aumentarDado();
+                  },
+                  child: Icon(Icons.arrow_right_rounded, size: 50),
+                ),
+              ],
             ),
           ],
         ),
